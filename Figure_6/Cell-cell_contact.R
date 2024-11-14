213574d68db968ae3028c408122abfa999a47f72
sabukar22 <161746819+sabukar22@users.noreply.github.com>
2024-11-14 11:30:52 +0000

    Update and rename Duration_calculation.R to Cell-cell_contact.R

--- Figure_6/Cell-cell_contact.R
+++ Figure_6/Cell-cell_contact.R
@@ -1,20 +1,15 @@
-setwd("D:/00_1st_Year_PhD/00_Manuscript_2/Live-imaging-cell-analysis/Cell_contacts")
-
 library(readxl)
 library(dplyr)
 library(writexl)
 
-folder_path <- "D:/00_1st_Year_PhD/00_Manuscript_2/Live-imaging-cell-analysis/Cell_contacts/16hrs"
+folder_path <- " "
 
 #file_list <- list.files(folder_path, pattern = "JSQH.*_Gen.*_contacts.xlsx", full.names = TRUE)
-file_list <- list.files(folder_path, pattern = "test.xlsx", full.names = TRUE)
-
 
 results <- data.frame(File = character(), Duration = numeric(), Name = character(), stringsAsFactors = FALSE)
 
 for (file in file_list) {
   df <- read_excel(file)
-  
   df_grouped <- df %>%
     group_by(File) %>%
     summarize(
@@ -22,10 +17,7 @@ for (file in file_list) {
       total_count = n(),
       Duration = (true_count / total_count) * 100
     )
-  
   results <- rbind(results, data.frame(File = df_grouped$File, Duration = df_grouped$Duration, Name = basename(file)))
 }
-
-#output_file <- "Durations_16hrs.xlsx"
-output_file <- "test2.xlsx"
+output_file <- "Durations_16hrs.xlsx"
 write_xlsx(results, output_file)
